94fd132fbef502dcb429e2da54ef364f050ff5b9
shreyanshyad <shreyansh.yadav.cse19@itbhu.ac.in>
2020-11-15 20:42:46 +0530

    Set up login and register

--- project/settings.py
+++ project/settings.py
@@ -124,3 +124,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'stocks-home'
+LOGIN_URL = 'login'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(
+        default='default_profile_image.jpg', upload_to='profile_images')
+
+    def __str__(self):
+        return f'{self.user.username} profile'
+
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+        if img.height > 256 or ima.width > 256:
+            output_size = (256, 256)
+            img.thumbnail(output_size)
+            img.save()
